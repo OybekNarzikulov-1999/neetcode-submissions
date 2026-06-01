8a81939b5cc37d15d0e4fec6f9d906c038a96b81
Oybek <oybeknarzikulov1999@gmail.com>
2026-06-01 13:01:37 +0500

    Add: duplicate-integer - submission-13

---
+++ Data Structures & Algorithms/duplicate-integer/submission-13.swift
@@ -0,0 +1,27 @@
+class Solution {
+    func hasDuplicate(_ nums: [Int]) -> Bool {
+        // 1. Brute force // TC: O(n2) SC: O(1)
+        // let count = nums.count
+        // for i in 0..<count {
+        //     for j in (i + 1)..<count {
+        //         if nums[i] == nums[j] {
+        //             return true
+        //         }
+        //     }
+        // }
+        // return false
+
+        // 2. Set
+        return nums.count != Set(nums).count
+
+        // 3. Hash Set
+        // var container = Set<Int>()
+        // for num in nums {
+        //     if container.contains(num) {
+        //         return true
+        //     }
+        //     container.insert(num)
+        // }
+        // return false
+    }
+}
