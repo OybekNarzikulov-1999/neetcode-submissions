61863cb4fb4ae653178ca542f017959c0f57a622
Oybek <oybeknarzikulov1999@gmail.com>
2026-06-08 17:56:16 +0500

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func groupAnagrams(_ strs: [String]) -> [[String]] {
+        var dic = [[String:Int]: [String]]()
+        var collection = [String:Int]()
+        for str in strs {
+            let array = Array(str)
+            for i in 0..<array.count {
+                collection[String(array[i]), default: 0] += 1 // Hash ==
+            }
+            dic[collection, default: []].append(str)
+            collection.removeAll()
+        }
+        return Array(dic.values)
+    }
+}
