79ec2225e213518c0bf88a88b3c86777239571ee
Oybek <oybeknarzikulov1999@gmail.com>
2026-06-09 18:29:58 +0500

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.swift
@@ -0,0 +1,31 @@
+import HeapModule
+
+struct NumFreq: Comparable {
+    let num: Int
+    let freq: Int
+
+    static func < (lhs: NumFreq, rhs: NumFreq) -> Bool {
+        return lhs.freq > rhs.freq
+    }
+}
+
+
+class Solution {
+    func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
+        var coll = [Int:Int]()
+        for i in 0..<nums.count {
+            coll[nums[i], default: 0] += 1
+        }
+        
+        var heap: Heap<NumFreq> = []
+        for (num, freq) in coll {
+            heap.insert(NumFreq(num: num, freq: freq))
+        }
+
+        var res = [Int]()
+        for i in 0..<k {
+            res.append(heap.removeMin().num)
+        }
+        return res
+    }
+}
